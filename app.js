c431fee85015ea82ac2eb593ec46e4f31ee7b992
Sava Reyhano <savareyhano99@gmail.com>
2022-12-06 11:37:55 +0700

    challange gambar dan css

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const app = express()
+const port = 3000
+
+app.use(express.static("public"));
+
+// pake ejs dan layout
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+app.set('layout', 'layout/layout')
+
+app.use((req, res, next) => {
+    console.log('Time:', Date.now())
+    next()
+})
+
+// buka halaman index.ejs
+app.get('/', (req, res) => {
+    res.render('index',
+    {
+        nama: 'AKW',
+        title: 'WebServer EJS',
+    })
+})
+
+// buka halaman about.ejs
+app.get('/about', (req, res) => {
+    res.render('about', {title: 'About'})
+})
+
+// buka halaman contact.ejs
+app.get('/contact', (req, res) => {
+    cont = [
+        {
+            name: 'AKW',
+            email: 'akw@qwe.com',
+        },
+        {
+            name: 'KWA',
+            email: 'kwaqwe.com',
+        },
+        {
+            name: 'WKA',
+            email: 'wka@qwe.com',
+        },
+    ]
+
+    // if (typeof cont !== 'undefined') {
+    //     res.render('contact', {
+    //         nama: 'AKW',
+    //         title: 'WebServer EJS',
+    //         cont,
+    //     })
+    // } else {
+    //     res.send('Error!')
+    // }
+
+    res.render('contact', {
+        nama: 'AKW',
+        title: 'Contact',
+        cont,
+    })
+})
+
+app.get('/product/:id', (req, res) => {
+    res.send('product id : ' + req.params.id + '<br></br>'
+        + 'category id : ' + req.query.idCat)
+})
+
+// diluar route diatas maka akan tampil halaman ini
+app.use('/', (req, res) => {
+    res.status(404)
+    res.send('404: Page not found!')
+})
+
+// nampilin di CLI "example app port 3000"
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
